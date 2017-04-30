95ac58614ea64aae1b76a967bd5f48a8b1cd1f1a
Barry de Kleijn <kleijn.barry@gmail.com>
2017-04-30 20:37:17 +0200

    Update setup

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": ["react-app", "xo-space/esnext", "xo-react"],
+  "rules": {
+    "object-curly-spacing": [
+      2,
+      "always"
+    ],
+    "react/jsx-indent": [
+      2,
+      2
+    ],
+    "space-before-function-paren": [
+      2,
+      {
+        "anonymous": "always",
+        "named": "always"
+      }
+    ]
+  }
+};
